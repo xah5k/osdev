de9fd8ef8a1ad9f12dbd543f2a8cdc9ad57db32d
ah5k <ah5k_alt02@proton.me>
2026-08-01 20:32:29 +0100

    ksyscall: well it isnt very visible in kernel but in mlibc theres access() now

--- user/hello/src/hello.c
+++ user/hello/src/hello.c
@@ -6,16 +6,11 @@
 
 int main(int argc, const char* argv[]) {
     setvbuf(stdout, NULL, _IONBF, 0);
-    DIR* f = opendir("initrd:/programs");
-    if (!f) {
-        printf("fail to open directory.\r\n");
-        return -1;
-    }
-    printf("acquire dir 0x%lx\r\n", f);
-    struct dirent* dent = readdir(f);
-    while (dent->d_type != 0) {
-        printf("dir: %d : %s\r\n", dent->d_type, dent->d_name);
-        dent = readdir(f);
-    }
+    int x = access("initrd:/programs", F_OK);
+    printf("result = %d after try access 'initrd:/programs'\r\n", x);
+    x = access("initrd:/nonexisting.txt", F_OK);
+    printf("result = %d after try access 'initrd:/nonexisting.txt'\r\n", x);
+    x = access("initrd:/hi.txt", F_OK);
+    printf("result = %d after try access 'initrd:/hi.txt'\r\n", x);
     return 0;
 }
